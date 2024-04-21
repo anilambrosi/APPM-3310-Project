7090fdb8480529613d65e571306650f24cc281ae
siraaj <siraajssandhu@gmail.com>
2024-04-21 06:51:10 -0600

    perron frobenius

--- src/common.py
+++ src/common.py
@@ -1,32 +1,30 @@
 # utils for cleaning/loading data
 
-import numpy as np
-import csv
+from csv import reader as csv_reader
 import re
-import matplotlib.pyplot as plt
+import math
+import numpy as np
 from io import StringIO
+
+sgn = lambda n: math.copysign(1, n)
 
 class Csv:
   def __init__(self, path: str):
     with open(path, mode='r') as file:
       text = file.read() # remove seed from team names
       clean_text = re.sub(r'\([0-9]+\)\s*', '', text)
-      csv_file = csv.reader(StringIO(clean_text))
+      csv_file = csv_reader(StringIO(clean_text))
       next(csv_file) # remove header
       self.data = [line for line in csv_file if all(entry != '' for entry in line)]
 
-def build_team_matrix(csv_obj):
-  teams = list(set(line[0] for line in csv_obj.data) | set(line[2] for line in csv_obj.data))
-  num_teams = len(teams)
-  team_matrix = np.zeros((num_teams, num_teams), dtype=float)
-  for line in csv_obj.data:
-    w, wpts, l, lpts = line
-    wi = teams.index(w)
-    li = teams.index(l)
-    team_matrix[wi, li] += float(wpts)
-    team_matrix[li, wi] += float(lpts)
-  
-  # testing
-  plt.imshow(team_matrix)
-  plt.colorbar()
-  plt.show()
+# find eigenvector correlating with largest eigenvalue of A
+def power_method(A: np.ndarray[np.float32], num_ierations: int) -> np.ndarray[np.float32]:
+  # find eigenvalue approximation
+  r = np.random.rand(A.shape[1])
+  for _ in range(num_ierations):
+    r = A @ r
+    r /= np.linalg.norm(r)
+
+  return r
+
+
--- src/perron_frobenius.py
+++ src/perron_frobenius.py
@@ -1,10 +1,82 @@
 # code for 1st paper (perron frobenius theorem)
 
-from common import Csv, build_team_matrix
+from common import Csv, power_method
+from math import copysign, sqrt, fabs, pi
+from dataclasses import dataclass
+from enum import Enum
+from functools import cmp_to_key
+import numpy as np
+from matplotlib import colormaps as cmaps
+import matplotlib.pyplot as plt
+
+class PrefMethod(Enum): 
+  DISCRETE = 'discrete'
+  INTERPOLATE = 'interpoate'
+  INTERPOLATE_OFFSET = 'interpolate_offset'
+  NONLINEAR = 'nonlinear'
+
+@dataclass
+class PerronFrobeniusSettings:
+  pref_method: PrefMethod
+  power_iterations: int
+
+calc_pref_interpolate = lambda si, sj: si / (si + sj)
+calc_pref_interpolate_offset = lambda si, sj: calc_pref_interpolate(si + 1, sj + 1)
+
+def calc_pref_discrete(si: int, sj: int) -> float:
+  if si == sj:
+    return 0.5
+  elif si > sj:
+    return 1
+  return 0
+
+def calc_pref_nonlinear(si: int, sj: int) -> float:
+  x = calc_pref_interpolate_offset(si, sj)
+  return 0.5 + 0.5 * copysign(1, x - 0.5) * sqrt(fabs(2 * x - 1))
+
+PREF_METHODS = {
+  PrefMethod.DISCRETE: calc_pref_discrete,
+  PrefMethod.INTERPOLATE: calc_pref_interpolate,
+  PrefMethod.INTERPOLATE_OFFSET: calc_pref_interpolate_offset,
+  PrefMethod.NONLINEAR: calc_pref_nonlinear
+}
+
+def make_pref_matrix(csv: Csv, settings: PerronFrobeniusSettings) -> tuple[list[str], np.ndarray[np.float32]]:
+  teams = list(set(line[0] for line in csv.data) | 
+               set(line[2] for line in csv.data))
+  num_teams = len(teams)
+
+  # make preference matrix
+  calc_a = PREF_METHODS[settings.pref_method]
+  pref_matrix = np.zeros((num_teams, num_teams), dtype=np.float32)
+  
+  for line in csv.data:
+    iname, ipts, jname, jpts = line
+
+    ipts = int(ipts)
+    jpts = int(jpts)
+    i = teams.index(iname)
+    j = teams.index(jname)
+
+    pref_matrix[i, j] = calc_a(ipts, jpts)
+    pref_matrix[j, i] = calc_a(jpts, ipts)
+
+  return (teams, pref_matrix)
 
 def main():
   csv = Csv('../data/games.csv')
-  build_team_matrix(csv)
+
+  settings = PerronFrobeniusSettings(
+    pref_method=PrefMethod.NONLINEAR, 
+    power_iterations=10)
+
+  teams, pref_matrix = make_pref_matrix(csv, settings)
+  num_teams = len(teams)
+  rank_vec = power_method(pref_matrix, settings.power_iterations)
+
+  team_ranks = sorted(zip(teams, rank_vec), key=cmp_to_key(lambda a, b: b[1] - a[1]))
+  for team, rank in team_ranks[:20]:
+    print(f'{team}: {rank}')
 
 if __name__ == '__main__':
   main()
